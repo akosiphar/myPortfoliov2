b961945bee346a6c2b7183113204fe200f90717f
akosiphar <raphgalag@gmail.com>
2023-04-14 11:18:58 +0800

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myPortfoliov2/',
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.PNG']
 })
