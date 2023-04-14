31078e8569f5248655e24762172b4c01ce2d60b1
akosiphar <raphgalag@gmail.com>
2023-04-14 11:32:47 +0800

    added basename to router

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-    <Router>
+    <Router basename='/myPortfoliov2'>
       <AppNavBar />
       <Routes>
         <Route path='/' element={<Homepage />} />
